7822a43eac55f0123a0d35fdd952b3d40c411a4a
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 22:21:10 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,55 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time >= 11
+    puts "You're late"
+    elsif time <= 11
+      puts "You're early!"
 end
+end
+simple_curfew_checker(12)
 
 def curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "It's before 11"
+  else
+   "It's after 11"
 end
+end
+curfew_checker(12)
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "You have time!"
+  elsif time == 11
+   "You're right on time!"
+  else
+     "You're late"
 end
+end
+complex_curfew_checker(12)
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  time = 8
+  curfew = 11
+  hours_left == curfew - time
+  puts "You have #{hours_left}"
+  if time == 11
+     "You're on time!"
+  elsif time > 11
+   "You're late"
+  elsif time < 11
+   "You have #{hours_left}"
 end
+end
+#deluxe_curfew_checker(12)
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  if current_time <= curfew_time
+    puts "You're chillin, have fun, you have time!"
+  elsif current_time == curfew_time
+     "Head back quick!"
+  elsif current_time >= curfew_time
+     "Hurry! You're late!"
 end
+end
+
+platinum_curfew_checker(8, 11)
